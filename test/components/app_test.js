6e844a5b4a0bf1d434bd220d1737a77c8c498f59
Merab <merabpkhaladze@yahoo.com>
2017-10-08 21:10:08 +0400

    test ul list

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -10,4 +10,7 @@ describe('App' , () => {
 	it('show a comment box', () => {
 		expect(component.find('.comment_box')).to.exist
 	})
+	it('show a comment list', () => {
+		expect(component.find('.comment_list')).to.exist
+	})
 });
---
+++ test/components/comment_list_test.js
@@ -0,0 +1,20 @@
+import { renderComponent, expect } from '../test_helper'
+import CommentList from '../../src/components/comment_list'
+
+describe('CommentList', () => {
+	let component
+
+	beforeEach(() => {
+		const props = { comments: ['New Comment', 'Other New Comment']}
+		component = renderComponent(CommentList, null, props)
+	})
+
+	it('show an li fro each comment', () => {
+		expect(component.find('li').length).to.equal(2)
+	})
+
+	it('show each comment that is provided', () => {
+		expect(component).to.contain('New Comment')
+		expect(component).to.contain('Other New Comment')
+	})
+})
